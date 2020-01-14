d2ac5fc895a29952198d692ca4ce6e9b75730fec
liszero <liszero@hotmail.com>
2020-01-14 15:40:17 +0800

    优化写法，去除没用的部分

--- commons/make_log.py
+++ commons/make_log.py
@@ -3,14 +3,13 @@ import logging
 
 def get_logger():
     # 获取logger实例，如果参数为空则返回root logger
-    logname = '%s.log' % time.strftime('%Y_%m_%d')
     logger = logging.getLogger()
     if not logger.handlers:
         # 指定logger输出格式
         formatter = logging.Formatter()
 
         # 文件日志
-        file_handler = logging.FileHandler(logname,encoding="utf8")
+        file_handler = logging.FileHandler("log",encoding="utf8")
         file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
 
         # 为logger添加的日志处理器
